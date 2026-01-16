b90c95634b2cb16171f2e814ea531860151a40d9
Brian van Vlymen <arduino731@gmail.com>
2026-01-15 18:29:54 -0600

    about

--- app/components/3Home.js
+++ app/components/3Home.js
@@ -3,18 +3,20 @@ import useHandleScroll from '../hooks/HandleScroll'
 const Home = () => {
     const currentSection = useHandleScroll();
     return (
-        <div id="Home" className={`scrollHandle transition-opacity duration-1000 ease-in-out colorBackground h-screen flex flex-col justify-center items-center text-center px-6
+        <div id="Home" className={`scrollHandle transition-opacity duration-1000 ease-in-out colorBackground h-screen flex flex-col justify-center items-center text-center px-3
         ${
         currentSection === 'home' ? 'opacity-100' : 'opacity-0'
         }`}
         data-id="home"
         >
-            <h1 className="text-4xl md:text-6xl colorTextOpposite font-bold mb-4 fadeIn">Hello, I am&nbsp;
+            <h1 className="text-4xl md:text-6xl colorTextOpposite font-extrabold mb-4">Hello, I am&nbsp;
                 <span className="uppercase colorText font-bold fadeIn">Brian van Vlymen</span>
             </h1>
-            <h1 className="text-4xl md:text-6xl colorTextOpposite font-bold mb-4 fadeIn">Hi, I'm a Frontend Developer</h1>
-            <p className="text-lg md:text-xl colorTextOpposite max-w-2xl fadeIn">
-                I build beautiful and functional websites using modern technologies.
+            <h1 className="text-2xl md:text-3xl colorTextOpposite font-semi mb-4 fadeIn">Full-Stack Developer & DevOps Engineer</h1>
+            <p className="max-w-2xl mx-auto text-lg md:text-xl colorText leading-relaxed colorTextOpposite  fadeIn">
+                I specialize in building <strong>high-performance web applications</strong> and 
+                <strong> optimized Linux infrastructure</strong>. Whether it's crafting GSAP animations 
+                or managing secure server environments, I deliver end-to-end digital solutions.
             </p>
 
             <div className="m-4">
@@ -22,7 +24,6 @@ const Home = () => {
                     <button className="fadeIn px-4 py-2 hoverSpotlight colorText colorBackgroundOpposite">View My Work</button>
                 </a>
             </div>
-
         </div>
         
     );
