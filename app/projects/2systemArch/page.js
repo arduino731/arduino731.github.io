bbf4f7c1be965b6ffc392640d5a8bdf99806e249
Brian van Vlymen <arduino731@gmail.com>
2026-01-28 18:35:08 -0600

    lint

--- app/projects/2systemArch/page.js
+++ app/projects/2systemArch/page.js
@@ -1,13 +1,13 @@
 'use client' // Required for useState
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import "../../globals.css"
 import Project2 from './Project2'
 import "./style.css"
-import NewTabComponent from './MERN/Mern' // The new component you want to show
+// import NewTabComponent from './MERN/Mern' // The new component you want to show
 
 
 export default function page() {
-  const [showNewTab, setShowNewTab] = useState(false);
+  // const [showNewTab, setShowNewTab] = useState(false);
   
   return (
     <section>
